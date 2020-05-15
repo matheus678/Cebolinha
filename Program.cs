986f82031859e13d375c347dfffad52205659bcc
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-15 20:28:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase para saber a tradução em cebolês");
+            string cebola = Console.ReadLine();
+            Console.WriteLine( cebola .Replace("r", "l"));
+        }
+    }
+}
